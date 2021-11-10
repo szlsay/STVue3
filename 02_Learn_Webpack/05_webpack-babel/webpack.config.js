756f008f00c7aef48a26c3c1f8daf2529f34a7d2
szlsay <13865738+szlsay@users.noreply.github.com>
2021-11-10 16:33:41 +0800

    05

---
+++ 02_Learn_Webpack/05_webpack-babel/webpack.config.js
@@ -0,0 +1,164 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const {DefinePlugin} = require("webpack");
+const CopyPlugin = require("copy-webpack-plugin");
+module.exports = {
+  // 设置模式
+  // development 开发阶段, 会设置development
+  // production 准备打包上线的时候, 设置production
+  mode: "development",
+  // 设置source-map, 建立js映射文件, 方便调试代码和错误
+  devtool: "source-map",
+  entry: "./src/main.js",
+  output: {
+    path: path.resolve(__dirname, "./build"),
+    filename: "js/bundle.js",
+    // assetModuleFilename: "img/[name]_[hash:6][ext]"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/, //正则表达式
+        // 1.loader的写法(语法糖)
+        // loader: "css-loader"
+
+        // 2.完整的写法
+        use: [
+          // {loader: "css-loader"}
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              esModule: false, //解决背景图乱码，生成多余文件问题
+              importLoaders: 1
+            }
+          },
+          // "css-loader",
+          "postcss-loader"
+          // {
+          //   loader: "postcss-loader",
+          //   options: {
+          //     postcssOptions: {
+          //       plugins: [
+          //         require("autoprefixer")
+          //       ]
+          //     }
+          //   }
+          // }
+        ]
+      },
+      {
+        test: /\.less$/,
+        use: [
+          "style-loader",
+          // "css-loader",
+          {
+            loader: "css-loader",
+            options: {
+              esModule: false, //解决背景图乱码，生成多余文件问题
+              importLoaders: 1
+            }
+          },
+          
+          "less-loader"
+        ]
+      },
+      // {
+      //   test: /\.(jpe?g|png|gif|svg)$/,
+      //   use: {
+      //     loader: "file-loader",
+      //     options: {
+      //       // outputPath: "img",
+      //       // name: "[name]_[hash:6].[ext]"
+      //       name: "img/[name]_[hash:6].[ext]"
+      //     }
+      //   }
+      // },
+
+      // {
+      //   test: /\.(jpe?g|png|gif|svg)$/,
+      //   use: {
+      //     loader: "url-loader",
+      //     options: {
+      //       // outputPath: "img",
+      //       name: "img/[name]_[hash:6].[ext]",
+      //       limit: 100 * 1024
+      //     }
+      //   }
+      // },
+
+      {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        type: "asset",
+        generator: {
+          filename: "img/[name]_[hash:6][ext]"
+        },
+        parser: {
+          dataUrlCondition: {
+            maxSize: 10 * 1024
+          }
+        }
+      },
+      // {
+      //   test: /\.(eot|ttf|woff2?)$/,
+      //   use: {
+      //     loader: "file-loader",
+      //     options: {
+      //       // outputPath: "font",
+      //       name: "font/[name]_[hash:6].[ext]"
+      //     }
+      //   }
+      // },
+      {
+        test: /\.(eot|ttf|woff2?)$/,
+        type: "asset/resource",
+        generator: {
+          filename: "font/[name]_[hash:6][ext]"
+        }
+      },
+      // {
+      //   test: /\.js$/,
+      //   use: {
+      //     loader: "babel-loader",
+      //     options: {
+      //       // plugins: [
+      //       //   "@babel/plugin-transform-arrow-functions",
+      //       //   "@babel/plugin-transform-block-scoping",
+      //       // ],
+      //       presets: [
+      //         "@babel/preset-env"
+      //       ]
+      //     }
+      //   }
+      // }
+      {
+        test: /\.js$/,
+        loader: "babel-loader"
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      title: "szlsay"
+    }),
+    new DefinePlugin({
+      BASE_URL: "'./'"
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: "public",
+          to: "./",
+          globOptions: {
+            ignore: [
+              "**/index.html"
+            ]
+          }
+        }
+      ]
+    })
+  ]
+}
