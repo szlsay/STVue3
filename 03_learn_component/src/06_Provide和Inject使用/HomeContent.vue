b035cbd9b25901ad22505008484b8bb61f887aad
szlsay <13865738+szlsay@users.noreply.github.com>
2021-11-15 08:32:30 +0800

    06

---
+++ 03_learn_component/src/06_Provide和Inject使用/HomeContent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    HomeContent: {{name}} - {{age}} - {{length.value}} - {{sex}}
+  </div>
+</template>
+
+<script>
+  export default {
+    inject: ["name", "age", "length", "sex"],
+  }
+</script>
+
+<style scoped>
+
+</style>
