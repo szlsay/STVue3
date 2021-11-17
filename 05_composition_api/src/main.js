ff99a90f2cc46cd83203f2be4c958e0198c5a945
szlsay <13865738+szlsay@users.noreply.github.com>
2021-11-17 09:38:49 +0800

    02

--- 05_composition_api/src/main.js
+++ 05_composition_api/src/main.js
@@ -1,4 +1,18 @@
 import { createApp } from 'vue'
-import App from './01_mixin和extends/App'
+import App from './02_compositionAPI基础/App'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+// app.mixin({
+//   data() {
+//     return {}
+//   },
+//   methods: {
+
+//   },
+//   created() {
+//     console.log("全局的created生命周期");
+//   }
+// });
+
+app.mount("#app");
