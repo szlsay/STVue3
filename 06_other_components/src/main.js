ab5b392e9552ef0d6cb382aaa74ee5222eeb75eb
szlsay <13865738+szlsay@users.noreply.github.com>
2021-11-22 09:26:02 +0800

    03

--- 06_other_components/src/main.js
+++ 06_other_components/src/main.js
@@ -1,4 +1,21 @@
 import { createApp } from 'vue'
-import App from './02_jsx的使用/App'
+import App from './03_自定义指令/App.vue'
+import registerDirectives from './directives'
+// import pluginObject from './plugins/plugins_object'
+// import pluginFunction from './plugins/plugins_function'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+registerDirectives(app);
+
+// app.use(pluginObject);
+// app.use(pluginFunction);
+
+// app.directive("focus", {
+//   mounted(el, bindings, vnode, preVnode) {
+//     console.log("focus mounted");
+//     el.focus();
+//   }
+// })
+
+app.mount('#app');
