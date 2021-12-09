b8415a23608b004cd918813809c3b6f49f47c80d
szlsay <409178030@qq.com>
2021-12-09 15:12:52 +0800

    connect

---
+++ 11-learn/02-connect/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="(title, index) in titles" :key="index">
+        <show-message :title="title" :isActive='currentIndex == index' @superadd="superadd"></show-message>
+      </li>
+    </ul>
+    <h2>{{suptitle}}</h2>
+  </div>
+</template>
+
+<script>
+import {ref} from 'vue'
+import ShowMessage from "./ShowMessage.vue";
+export default {
+  components: { ShowMessage },
+  setup(props) {
+    const currentIndex = ref(0) 
+    const titles = ref(['衣服', '鞋子', '裤子'])
+    const superadd = ((title) => {
+      currentIndex.value = titles.value.indexOf(title)
+      suptitle.value = title
+    })
+    const suptitle = ref(titles.value[0])
+    return {
+      titles,currentIndex,superadd,suptitle
+    }
+  },
+};
+</script>
+
+<style scoped>
+li {
+  display: inline-block;
+  width: 100px;
+  height: 50px;
+  list-style: none;
+}
+
+</style>
---
+++ 11-learn/02-connect/ShowMessage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <span @click="subadd" :class="{active: isActive}">{{ title }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  emits: ['superadd'],
+  props: ["title", 'isActive'],
+  setup(props, context) {
+    console.log(props.title)
+    const subadd = (() => context.emit('superadd', props.title))
+    return {
+      subadd
+    }
+  }
+};
+</script>
+
+<style scoped>
+.active{
+  color: red;
+}
+</style>
