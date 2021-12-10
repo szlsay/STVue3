02c66510ca81929f558e5907d7ff78c41d67d781
szlsay <409178030@qq.com>
2021-12-10 17:50:53 +0800

    Create 02-03.ts

---
+++ 11-learn/03-TS/02-03.ts
@@ -0,0 +1,14 @@
+let str = 'seven';
+// str = 7;
+console.log(str)
+let str1: string = 'seven';
+// str1 = 7;
+console.log(str)
+let str2: String = 'seven';
+// str2 = 7;
+console.log(str)
+
+let myFavoriteNumber;
+myFavoriteNumber = 'seven';
+myFavoriteNumber = 7;
+console.log(myFavoriteNumber)
