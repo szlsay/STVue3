1defbdb8ffb741b28ad1b32f34062fcd9ae8854c
szlsay <409178030@qq.com>
2021-12-11 15:26:11 +0800

    Create 02-10.ts

---
+++ 11-learn/03-TS/02-10.ts
@@ -0,0 +1,12 @@
+let b: Boolean = new Boolean(1);
+let e: Error = new Error('Error occurred');
+let d: Date = new Date();
+let r: RegExp = /[a-z]/;
+
+console.log(b)
+console.log(e)
+console.log(d)
+console.log(r)
+
+// 类型“string”的参数不能赋给类型“number”的参数
+// Math.pow(10, '2');
