343bf28cc7da8f83f5d71927cd1c5c259012de55
小沈曰 <409178030@qq.com>
2021-12-14 23:50:02 +0800

    init-vue3

---
+++ 11-learn/04-vue3/vuedemo/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <div>x轴：{{ x }}</div>
+    <div>y轴：{{ y }}</div>
+    <div>
+      count: {{ count }}
+      <button @click="add">增加</button>
+    </div>
+  </div>
+</template>
+
+<script>
+  import { reactive, toRefs, ref } from "vue";
+  import { onMounted, onUnmounted } from "vue";
+  export default {
+    setup() {
+      const mouse = reactive({
+        x: 0,
+        y: 0,
+      });
+      const move = (e) => {
+        mouse.x = e.pageX;
+        mouse.y = e.pageY;
+      };
+      onMounted(() => {
+        document.addEventListener("mousemove", move);
+      });
+      onUnmounted(() => {
+        document.removeEventListener("mousemove", move);
+      });
+
+      const count = ref(0);
+      const add = () => count.value++;
+      return {
+        ...toRefs(mouse),
+        count,
+        add
+      };
+    },
+  };
+</script>
+
+<style lang="scss" scoped></style>
---
+++ 11-learn/04-vue3/vuedemo/src/App01.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div>x轴：{{ x }}</div>
+    <div>y轴：{{ y }}</div>
+    <div>count: {{count}}
+        <button @click="add">增加</button>
+    </div>
+  </div>
+</template>
+
+<script>
+  import { ref } from "vue";
+
+  export default {
+    setup() {
+      const x = ref(0);
+      const y = ref(0);
+      const count = ref(0);
+      const move = (e) => {
+        // console.log(e);
+        x.value = e.pageX
+        y.value = e.pageY
+      };
+      const add = () => count.value++
+      return { x, y, move, count,  add};
+    },
+    mounted() {
+      console.log("mounted");
+      document.addEventListener('mousemove', this.move);
+    },
+    destroyed() {
+        document.removeEventListener('mousemove', this.move)
+    },
+  };
+</script>
+
+<style lang="scss" scoped></style>
