8e2c2188a19cbe21e078df7220b67146d111fa40
szlsay <409178030@qq.com>
2021-12-31 17:27:14 +0800

    bill-view

--- 13-vant/vantaccountapp/src/router/index.ts
+++ 13-vant/vantaccountapp/src/router/index.ts
@@ -1,14 +1,18 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 
-const routes:RouteRecordRaw[] = [
+const routes: RouteRecordRaw[] = [
+  {
+    path: "/user",
+    component: () => import("@/views/login/index.vue"),
+  },
   {
     path: "/",
-    component: () => import("@/views/login/index.vue")
-  }
-]
+    component: () => import("@/views/bill/index.vue"),
+  },
+];
 const route = createRouter({
-  history: createWebHashHistory(), 
-  routes
-})
+  history: createWebHashHistory(),
+  routes,
+});
 
-export default route
+export default route;
---
+++ 13-vant/vantaccountapp/src/views/bill/index.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="bill-header">
+    <div class="total">
+      <div>
+        <span>总支出：</span>
+        <span class="total-count">￥12.34</span>
+      </div>
+      <div style="margin-left: 15px;">
+        <span>总收入：</span>
+        <span class="total-count">￥12.34</span>
+      </div>
+    </div>
+    <div class="select-type-date">
+    全部类型
+    </div>
+  </div>
+</template>
+
+<style lang="less" scoped>
+.bill-header {
+  background-color: #007fff;
+  color: white;
+  font-size: 14px;
+
+  .total{
+    display: flex;
+    justify-content: flex-start;
+    padding: 10px 20px;
+    &-count{
+      font-size: 20px;
+      font-weight: bold;
+    }
+  }
+}
+</style>
