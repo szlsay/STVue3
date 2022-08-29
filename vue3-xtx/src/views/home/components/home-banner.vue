f070dcd28df1312eb9e9126d0c613b493368a36b
小沈曰 <409178030@qq.com>
2022-08-29 21:50:49 +0800

    home

---
+++ vue3-xtx/src/views/home/components/home-banner.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home-banner">
+    <XtxCarousel auto-play :sliders="sliders" />
+  </div>
+</template>
+<script setup>
+import { ref } from 'vue'
+import { findBanner } from '@/api/home'
+import XtxCarousel from '@/components/library/xtx-carousel.vue'
+
+// 获取轮播图数据
+const sliders = ref([])
+findBanner().then((data) => {
+  sliders.value = data.result
+})
+</script>
+<style scoped lang="less">
+.home-banner {
+  width: 1240px;
+  height: 500px;
+  position: absolute;
+  left: 0;
+  top: 0;
+  z-index: 98;
+}
+// 覆盖样式
+.xtx-carousel {
+  :deep(.carousel-btn.prev) {
+    left: 270px;
+  }
+  :deep(.carousel-indicator) {
+    padding-left: 250px;
+  }
+}
+</style>
--- vue3-xtx/src/views/home/index.vue
+++ vue3-xtx/src/views/home/index.vue
@@ -1,9 +1,18 @@
 <template>
-home
+  <!-- 首页入口 -->
+  <div class="home-entry">
+    <div class="container">
+      <!-- 左侧分类 -->
+      <home-category></home-category>
+      <!-- 轮播图 -->
+      <HomeBanner />
+    </div>
+  </div>
 </template>
 
 <script setup>
-
+import HomeCategory from './components/home-category'
+import HomeBanner from './components/home-banner'
 </script>
 
 <style lang="less" scoped>
